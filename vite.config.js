658a253ae8dc13dce677992816de881b686ddbcb
dralexander0805 <69057498+dralexander0805@users.noreply.github.com>
2025-06-28 14:35:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Matt-Site/', // 👈 this tells Vite where your app is hosted
   plugins: [react()],
-})
+});
